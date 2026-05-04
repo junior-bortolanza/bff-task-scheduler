3d1442918c132ee7e93f13f4a1fc6e6c5beff4f9
Junior Bortolanza <gbortolanzajr@gmail.com>
2026-05-04 09:28:44 -0300

    added email client

---
+++ bff-agendador-tarefas/src/main/java/com/bortolanza/bff_agendadortarefas/infrastructure/client/EmailClient.java
@@ -0,0 +1,14 @@
+package com.bortolanza.bff_agendadortarefas.infrastructure.client;
+
+
+import com.bortolanza.bff_agendadortarefas.business.dto.out.TasksDTOResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "notification", url = "${notification.url}")
+public interface EmailClient {
+
+
+
+    void sendEmail(@RequestBody TasksDTOResponse dto);
+}
